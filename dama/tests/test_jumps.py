3a678dbbecc8fad7338b8f18a1c80d3d99b2d8e5
Emanuel Raad <raademang@gmail.com>
2019-11-17 17:12:19 -0500

    testing for normal piece jumps

--- dama/tests/test_jumps.py
+++ dama/tests/test_jumps.py
@@ -1,39 +1,21 @@
 from dama.game import dama
 from dama.agents import player
 from dama.game.constants import Color
+from dama.tests import gameboards
 import numpy as np
+import pprint
 
-class Test():
+class base_model():
     def __init__(self, player, board):
         self.player = player
         self.board = board
         self.damagame = dama.DamaGame()
         self.damagame.gameboard = board
-    def show_jumps(self, position):
-        res = self.damagame.get_piece_legal_move(self.player, position)
-        res['tree'].show(data_property='position')
-
-branching = np.array([
-            [3, 0, 3, 0, 3, 0, 3, 0], #0
-            [0, 3, 0, 3, 0, 3, 0, 3], #1
-            [3, 0, 3, 0, 3, 0, 3, 0], #2
-            [0, 3, 0, 3, 0, 3, 0, 3], #3
-            [3, 0, 3, 1, 3, 0, 3, 0], #4
-            [0, 3, 0, 3, 0, 3, 0, 3], #5
-            [0, 0, 0, 0, 0, 0, 0, 0], #6
-            [0, 0, 0, 0, 0, 0, 0, 0]  #7
-        ])
-
-branching2 = np.array([
-            [1, 0, 1, 0, 1, 0, 1, 0], #0
-            [0, 1, 0, 1, 0, 1, 0, 1], #1
-            [1, 0, 1, 0, 1, 0, 1, 0], #2
-            [0, 1, 0, 1, 0, 1, 0, 1], #3
-            [1, 0, 1, 3, 1, 0, 1, 0], #4
-            [0, 1, 0, 1, 0, 1, 0, 1], #5
-            [0, 0, 0, 0, 0, 0, 0, 0], #6
-            [0, 0, 0, 0, 0, 0, 0, 0]  #7
-        ])
+    def calc(self, position):
+        self.res = self.damagame.get_piece_legal_move(self.player, position)
+        # self.res['tree'].show(data_property='position')
+    def __eq__(self, other):
+        return self.res['tree'].show(data_property='position') == other.res['tree'].show(data_property='position')
 
 if __name__ == '__main__':
     
@@ -42,8 +24,27 @@ if __name__ == '__main__':
     white = player.Player(Color.WHITE)
     black = player.Player(Color.BLACK)
 
-    test1 = Test(white, branching)
-    test1.show_jumps(np.array([3, 3]))
+    test1 = base_model(white, gameboards.branching_white)
+    test1.calc(np.array([3, 3]))
+    print(test1.res['tree'].paths_to_leaves())
+    # test1.res['tree'].save2file('filename.txt', data_property='position')
 
-    test2 = Test(black, branching2)
-    test2.show_jumps(np.array([4, 3]))
+    test2 = base_model(black, gameboards.branching_black)
+    test2.calc(np.array([4, 3]))
+    # print(test2.res['tree'].paths_to_leaves())
+
+    tag_paths = []
+
+    for i in test2.res['tree'].paths_to_leaves():
+        path = []
+        for j in i:
+            path.append(test2.res['tree'].get_node(j).data.tag)
+            # print("{} ".format(test2.res['tree'].get_node(j).data.tag)),
+        tag_paths.append(path)
+        # print()
+    
+    for i in tag_paths:
+        print(i)
+
+    s = set(map(tuple, tag_paths))
+    s2 = set(map(tuple, tag_paths))
